cfcd421319d898cd0e5775b6c5816003807ddabd
Nick Mertin <nickmertin@gmail.com>
2018-05-10 10:26:30 -0400

    Set up actual game project

---
+++ Jonestown Adventure/Program.cs
@@ -0,0 +1,18 @@
+﻿using Mertin.RPG.Engine;
+using Mertin.RPG.Engine.DataModel;
+using System;
+using System.Windows;
+
+namespace Jonestown
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(String[] args)
+        {
+            var app = new Application { ShutdownMode = ShutdownMode.OnMainWindowClose };
+            var config = Application.LoadComponent(new Uri("/Config.xaml", UriKind.Relative)) as Config;
+            app.Run(new GameWindow(new GameEngine(config)));
+        }
+    }
+}
