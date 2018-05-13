226d91daccddc0934dde355a1fbae716e6a5a635
Nick Mertin <nickmertin@gmail.com>
2018-05-13 18:25:58 -0400

    Implemented Prompt event

---
+++ RPG Game Engine/DataModel/Prompt.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Markup;
+
+namespace Mertin.RPG.Engine.DataModel
+{
+    [ContentProperty("DisplayText")]
+    public sealed class Prompt : Event
+    {
+        public string PromptText { get; set; }
+
+        public string StoreKey { get; set; }
+
+        public string Regex { get; set; }
+
+        public string NextKey { get; set; }
+    }
+}
--- RPG Game Engine/GameEngine.cs
+++ RPG Game Engine/GameEngine.cs
@@ -1,4 +1,7 @@
 ﻿using Mertin.RPG.Engine.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Media;
 
@@ -7,18 +10,22 @@ namespace Mertin.RPG.Engine
     public sealed class GameEngine
     {
         Config config;
+        Dictionary<string, string> storedVars = new Dictionary<string, string>();
+        Regex processRegex = new Regex("{{([^{}]+)}}", RegexOptions.Compiled);
 
         public GameEngine(Config config)
         {
             this.config = config;
         }
 
+        private string Process(string text) => processRegex.Replace(text, match => storedVars.ContainsKey(match.Groups[1].Value) ? storedVars[match.Groups[1].Value] : match.Value);
+
         public void Run(GameWindow window)
         {
             var current = config.Events[config.StartEventKey];
             while (true)
             {
-                window.WriteLine(current.DisplayText, Colors.Yellow);
+                window.WriteLine(Process(current.DisplayText), Colors.Yellow);
                 switch (current)
                 {
                     case Info i:
@@ -28,11 +35,15 @@ namespace Mertin.RPG.Engine
                         current = config.Events[i.NextKey];
                         break;
                     case Fork f:
-                        window.WriteLine(f.Question + "\n", Colors.LightGreen);
+                        window.WriteLine(Process(f.Question) + "\n", Colors.LightGreen);
                         for (int i = 0; i < f.Options.Count; ++i)
-                            window.WriteLine($"  - [{i}]: {f.Options[i].Text}", Colors.Yellow);
+                            window.WriteLine($"  - [{i}]: {Process(f.Options[i].Text)}", Colors.Yellow);
                         window.WriteLine("", new Color());
-                        current = config.Events[f.Options[window.Get("What would you like to do? ", i => i < f.Options.Count && i >= 0, int.Parse)].NextKey];
+                        current = config.Events[f.Options[window.Get("Choose an option: ", i => i < f.Options.Count && i >= 0, int.Parse)].NextKey];
+                        break;
+                    case Prompt p:
+                        storedVars[p.StoreKey] = window.Get(Process(p.PromptText) + " ", string.IsNullOrEmpty(p.Regex) ? new Predicate<string>(_ => true) : new Regex(p.Regex).IsMatch, _ => _);
+                        current = config.Events[p.NextKey];
                         break;
                     case GameOver g:
                         window.WriteLine(g.Win ? "You won!" : "You lost!", Colors.Aqua);
